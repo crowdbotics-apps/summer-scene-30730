1528d9cf625466e69afa7e3d6abc01256d847b60
Crowdbotics <team@crowdbotics.com>
2021-09-22 00:31:10 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "summer_scene_30730Identifier",
+  url: "https://summer_scene_30730Identifier.botics.co",
+  api: "https://summer_scene_30730Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
